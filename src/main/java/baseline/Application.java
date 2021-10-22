23476d1de494759b583615dc06068179a0cf8c5e
Alexander <azoomer2@gmail.com>
2021-10-22 12:46:37 -0400

    Initial Commit

---
+++ src/main/java/baseline/Application.java
@@ -0,0 +1,10 @@
+package baseline;
+
+import javafx.stage.Stage;
+
+public class Application extends javafx.application.Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
